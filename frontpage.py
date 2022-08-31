90586541f10cbefe175159788bb863d799c8fcd1
Bhosaleneha <105287061+Bhosaleneha@users.noreply.github.com>
2022-08-31 12:03:39 +0530

    Create frontpage.py
    front page

---
+++ frontpage.py
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.rtl.min.css" integrity="sha384-+4j30LffJ4tgIMrq9CwHvn0NjEvmuDCOfk6Rpg2xg7zgOxWWtLtozDEEVvBPgHqE" crossorigin="anonymous">
+
+    <title>products details</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-light bg-light">
+   <a class="navbar-brand">ECOMMERCE</a>
+   <form class="form-inline">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">login </button>
+     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">sign up</button>
+   </form>
+  </nav>
+  <div class="container"></div>
+  <div class="card-group">
+  <div class="card">
+    <img src="https://images.unsplash.com/photo-1553103291-346188e304d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80 " class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">laptop</h5>
+      <h6>category: laptop</h6>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+     
+      <h6>category: laptop</h6><h6>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://images.unsplash.com/photo-1552501684-c441323557ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">laptop</h5>
+      <h6>category: laptop</h6>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+    <h6>category: laptop</h6>
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://images.unsplash.com/photo-1552415183-52af5aef5816?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">laptop</h5>
+      <h6>category: laptop</h6>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+    <h6>category: laptop</h6>
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+ </div>
+ </div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
